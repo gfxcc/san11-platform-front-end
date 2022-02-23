e22f56528fc3c205bad58c4e838f01a9a9669b1b
yong <ycao181@gmail.com>
2022-02-22 22:30:24 -0800

    Update endpoint for backend

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    public static serverIp: string = "http://0.0.0.0";
+    public static serverIp: string = "https://san11pk.org";
     public static domain: string = 'san11pk';
     public static san11ServerUrl: string = GlobalConstants.serverIp + ':8090';
     public static fileServerUrl: string = 'https://storage.googleapis.com/san11-resources';
